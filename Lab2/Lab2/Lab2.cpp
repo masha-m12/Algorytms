dc866ef7db8e70acdf2185c60f386da4466c8ec5
masha-m12 <mary.sun.light.too@gmail.com>
2024-10-10 18:35:39 +0300

    добавлено пространство имен, исправлены функции (без дублирования кода)

--- Lab2/Lab2/Lab2.cpp
+++ Lab2/Lab2/Lab2.cpp
@@ -2,75 +2,70 @@
 #include <string>
 #include <vector>
 
-// создание таблицы смещений
-void CreateTable(const std::string& pattern, std::vector<int>& table) {
-    int pattern_size = pattern.length();
-    for (int i = 0; i < 256; ++i) {
-        table[i] = pattern_size; // инициализируем таблицу значением длины образа
+namespace BoyerMoore {
+
+    // создание таблицы смещений
+    void CreateTable(const std::string& pattern, std::vector<int>& table) {
+        int pattern_size = pattern.length();
+        for (int i = 0; i < 256; ++i) {
+            table[i] = pattern_size; // инициализируем таблицу значением длины образа
+        }
+        for (int i = 0; i < pattern_size - 1; i++) {
+            table[static_cast<unsigned char>(pattern[i])] = pattern_size - 1 - i; // заполняем таблицу
+        }
     }
-    for (int i = 0; i < pattern_size - 1; i++) {
-        table[static_cast<unsigned char>(pattern[i])] = pattern_size - 1 - i; // заполняем таблицу
-    }
-}
 
-// поиска первого вхождения подстроки в строке
-int BoyerMoore_SearchFirst(const std::string& text, const std::string& pattern) {
-    int text_size = text.length();
-    int pattern_size = pattern.length();
-    if (pattern_size == 0 || text_size < pattern_size) return -1;
+    // поиск подстроки с возможностью указать вхождения
+    std::vector<int> Search(const std::string& text, const std::string& pattern, int start, int end, bool searchFirst) {
+        std::vector<int> indices;
+        int text_size = text.length();
+        int pattern_size = pattern.length();
 
-    std::vector<int> table(256);
-    CreateTable(pattern, table);
-
-    int bias = 0; // смещение
-    while (bias <= text_size - pattern_size) {
-        int j = pattern_size - 1;
-
-        while (j >= 0 && pattern[j] == text[bias + j]) {
-            --j;
+        if (pattern_size == 0 || pattern_size > text_size || start < 0 || end >= text_size || start > end) {
+            return indices;
         }
 
-        if (j < 0) {
-            return bias; // первое вхождение
+        std::vector<int> table(256);
+        CreateTable(pattern, table);
+
+        int bias = start; // начальное смещение
+        while (bias <= end - pattern_size) {
+            int j = pattern_size - 1;
+
+            while (j >= 0 && pattern[j] == text[bias + j]) {
+                --j;
+            }
+
+            if (j < 0) {
+                indices.push_back(bias); // сохраняем индекс вхождения
+                if (searchFirst) {
+                    return indices; // возвращаем сразу, если нужно только первое вхождение
+                }
+                bias++;
+            }
+            else {
+                bias += std::max(1, j - table[static_cast<unsigned char>(text[bias + j])]);
+            }
+        }
+        return indices;
+    }
+
+    // поиск первого вхождения подстроки в строке
+    int SearchFirst(const std::string& text, const std::string& pattern) {
+        auto indices = Search(text, pattern, 0, text.length() - 1, true);
+        if (indices.empty()) {
+            return -1;
         }
         else {
-            bias += std::max(1, j - table[static_cast<unsigned char>(text[bias + j])]);
-        }
-    }
-    return -1; // вхождение не найдено
-}
-
-// поиск всех вхождений подстроки в заданном диапазоне
-std::vector<int> BoyerMoore_SearchAll(const std::string& text, const std::string& pattern, int start, int end) {
-    std::vector<int> indices;
-    int text_size = text.length();
-    int pattern_size = pattern.length();
-
-    if (pattern_size == 0 || pattern_size > text_size || start < 0 || end >= text_size || start > end) {
-        return indices; // некорректные границы => пустой вектор
-    }
-
-    std::vector<int> table(256);
-    CreateTable(pattern, table);
-
-    int bias = start; // начальное смещение
-    while (bias <= end - pattern_size) {
-        int j = pattern_size - 1;
-
-        while (j >= 0 && pattern[j] == text[bias + j]) {
-            --j;
-        }
-
-        if (j < 0) {
-            indices.push_back(bias); // сохраняем индекс вхождения
-            bias++;
-        }
-        else {
-            bias += std::max(1, j - table[static_cast<unsigned char>(text[bias + j])]);
+            return indices[0];
         }
     }
 
-    return indices;
+    // поиск всех вхождений подстроки в заданном диапазоне
+    std::vector<int> SearchAll(const std::string& text, const std::string& pattern, int start, int end) {
+        return Search(text, pattern, start, end, false);
+    }
+
 }
 
 int main() {
@@ -84,9 +79,10 @@ int main() {
     std::getline(std::cin, pattern);
 
     // поиск первого вхождения
-    int firstIndex = BoyerMoore_SearchFirst(text, pattern);
+    int firstIndex = BoyerMoore::SearchFirst(text, pattern);
     if (firstIndex == -1) {
-        std::cout << "Вхождения нет" << '\n';
+        std::cout << "Вхождений нет" << '\n';
+        return 0;
     }
     else {
         std::cout << "Индекс первого вхождения подстроки: " << firstIndex << '\n';
@@ -97,7 +93,7 @@ int main() {
     int start, end;
     std::cin >> start >> end;
 
-    std::vector<int> allIndex = BoyerMoore_SearchAll(text, pattern, start, end);
+    std::vector<int> allIndex = BoyerMoore::SearchAll(text, pattern, start, end);
     std::cout << "Индексы всех вхождений подстроки: ";
 
     if (allIndex.empty()) {
